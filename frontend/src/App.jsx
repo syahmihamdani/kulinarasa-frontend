19643747a57700fd65f508bd7258180da36f76a2
HendhyW <hendhywirawan@gmail.com>
2025-05-10 17:35:20 +0700

    init front end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from './components/LoginPage';
+import HomePage from './components/HomePage';
+import RegisterPage from './components/RegisterPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* redirect ke login */}
+        {/* <Route
+          path="*"
+          element={<Navigate to="/login" replace />}
+        /> */}
+
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route path="/" element={<HomePage/>}/>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App

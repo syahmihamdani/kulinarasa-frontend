1315c9142229d561588dc3b40a4bd87858772f0e
RifatFaqih <160085950+RifatFaqih@users.noreply.github.com>
2025-05-20 18:26:56 +0700

    Delete kulinarasa-FinPro-main directory

--- kulinarasa-FinPro-main/README.md
+++
@@ -1,1 +0,0 @@
-# kulinarasa-FinPro
--- kulinarasa-FinPro-main/backend/src/utils/baseResponse.util.js
+++
@@ -1,6 +0,0 @@
-const baseResponse = (res, success,  status, message, payload) => {
-    return res.status(status).json({
-        success, message, payload });
-};
-
-module.exports = baseResponse;  
